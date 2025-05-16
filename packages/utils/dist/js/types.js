3f37646bda2c50656ab0183c1cb273205eaf5f35
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2025-05-17 01:49:59 +0300

    [utils] fix

--- packages/utils/dist/js/types.js
+++ packages/utils/dist/js/types.js
@@ -8,3 +8,37 @@ export {};
  * @typedef {() => T} Getter
  * @template T
  */
+/**
+ * @typedef {T extends void ? () => void : (arg: T) => void} Action
+ * @template [T=void]
+ */
+/**
+ * @typedef {(<T>() => T extends X ? 1 : 2) extends
+ *     (<T>() => T extends Y ? 1 : 2) ? A : B} IfEquals
+ * @template X
+ * @template Y
+ * @template A
+ * @template [B=never]
+ */
+/**
+ * @typedef {IfEquals<
+ *     { [P in K]: T[P] },
+ *     { -readonly [P in K]: T[P] },
+ *     false,
+ *     true
+ * >} IsReadonly
+ * @template T
+ * @template {keyof T} K
+ */
+/**
+ * @typedef {{
+ *     [K in keyof T as T[K] extends (...args: unknown[]) => unknown ? K : never]: T[K];
+ * }} MethodsOf
+ * @template T
+ */
+/**
+ * @typedef {{
+ *     [K in keyof T as IsReadonly<T, K> extends true ? K : never]: T[K];
+ * }} ReadonlyProps
+ * @template T
+ */
--- packages/utils/dist/types/types.d.ts
+++ packages/utils/dist/types/types.d.ts
@@ -1,3 +1,16 @@
 export type ConditionFn = () => boolean;
 export type Predicate<T> = (value: T) => boolean;
 export type Getter<T> = () => T;
+export type Action<T = void> = T extends void ? () => void : (arg: T) => void;
+export type IfEquals<X, Y, A, B = never> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? A : B;
+export type IsReadonly<T, K extends keyof T> = IfEquals<{
+    [P in K]: T[P];
+}, {
+    -readonly [P in K]: T[P];
+}, false, true>;
+export type MethodsOf<T> = {
+    [K in keyof T as T[K] extends (...args: unknown[]) => unknown ? K : never]: T[K];
+};
+export type ReadonlyProps<T> = {
+    [K in keyof T as IsReadonly<T, K> extends true ? K : never]: T[K];
+};
