35278ed4ebcb4dfc895faf87f0e171c7137a0f00
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2025-05-30 17:59:04 +0300

    [utils] add `MaybePromise` type

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -3,6 +3,7 @@ export type Predicate<T> = (value: T) => boolean;
 export type Getter<T> = () => T;
 export type Setter<T> = (value: T) => void;
 export type Action<T = void> = T extends void ? () => void : (arg: T) => void;
+export type MaybePromise<T> = T | Promise<T>;
 
 export type IfEquals<X, Y, A, B = never> = (<T>() => T extends X ? 1 : 2) extends
     (<T>() => T extends Y ? 1 : 2) ? A : B;
