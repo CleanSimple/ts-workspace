9fbd80d2bafbc513d23bce462cd834bceb700f59
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2025-12-04 19:44:08 +0200

    [misc] update sample userscript project

--- project-templates/userscript/src/UI/TestSpan.tsx
+++
@@ -1,8 +0,0 @@
-import type { FunctionalComponent } from '@cleansimple/plain-jsx';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const TestSpan: FunctionalComponent = (props) => {
-    return <span>Test span</span>;
-};
-
-export { TestSpan };
--- project-templates/userscript/src/main.tsx
+++ project-templates/userscript/src/main.tsx
@@ -1,14 +1,10 @@
-import { render } from '@cleansimple/plain-jsx';
 import { sleep } from '@cleansimple/utils-js';
-import { TestUI } from './UI/TestUI';
 
 async function main() {
     // comment
     console.info('Hi!');
     await sleep(1000);
     console.info('Bye!');
-
-    void render(document.body, <TestUI />);
 }
 
 void main();
