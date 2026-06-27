1e65dfd1ff9db52374dca3eac7e89e71154b4488
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2026-06-27 18:26:44 +0300

    [misc] lock dependencies to exact version

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -5,18 +5,36 @@ module.exports = defineConfig({
     async constraints({ Yarn }) {
 
         /**
+         * ensures that all workspaces use the same version as the root workspace
          * @param {string} dependencyName
-         * @param {string} version
          */
-        function enforce_version(dependencyName, version) {
-            for (const dep of Yarn.dependencies({ ident: dependencyName })) {
-                dep.update(version);
+        function enforceVersion(dependencyName) {
+            const deps = Yarn.dependencies({ ident: dependencyName });
+            const rootDep = deps.find(dep => dep.workspace.cwd === '.');
+            if (!rootDep) {
+                throw new Error(`Failed to enforce version for ${dependencyName}`);
+            }
+
+            for (const dep of deps) {
+                if (dep === rootDep)
+                    continue;
+                if (dep.type === "peerDependencies")
+                    continue;
+
+                dep.update(rootDep.range);
             }
         }
 
-        enforce_version("dprint", "0.50.2");
-        enforce_version("eslint", "9.39.1");
-        enforce_version("rollup", "4.52.4");
-        enforce_version("typescript", "5.9.3");
+        // locks dependencies to exact version
+        for (const dep of Yarn.dependencies()) {
+            if (dep.type === "peerDependencies")
+                continue
+            dep.update(dep.range.replace(/^[*^~]/, ''));
+        }
+
+        enforceVersion("dprint");
+        enforceVersion("eslint");
+        enforceVersion("rollup");
+        enforceVersion("typescript");
     },
 });
